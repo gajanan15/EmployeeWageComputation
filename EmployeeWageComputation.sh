fbf6fca4cfe9f0cf0796075b845e31dd45bdf289
gajanan15 <gajanandesai97@gmail.com>
2020-01-23 14:34:30 +0530

    ADDED:variable added and for loop added

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -4,17 +4,25 @@ echo "Welcome to Employee Wage Computation"
 WORKING_PARTTIME=1
 WORKING_FULLTIME=2
 EMPLOYEE_RATE_PER_HR=20
-employeeCheck=$((RANDOM%3))
+NUMBER_OF_WORKING_DAYS=20
 
-case $employeeCheck in
-	$WORKING_FULLTIME)
-		EMPLOYEE_HRS=8
-		;;
-	$WORKING_PARTTIME)
-		EMPLOYEE_HRS=4
-		;;
-	*)
-		EMPLOYEE_HRS=0
-		;;
-esac
-salary=$(($EMPLOYEE_HRS*$EMPLOYEE_RATE_PER_HR))
+#Variable
+totalSalary=0
+for((day=1;day<=$NUMBER_OF_WORKING_DAYS;day++))
+do
+	employeeCheck=$((RANDOM%3))
+
+	case $employeeCheck in
+		$WORKING_FULLTIME)
+			EMPLOYEE_HRS=8
+			;;
+		$WORKING_PARTTIME)
+			EMPLOYEE_HRS=4
+			;;
+		*)
+			EMPLOYEE_HRS=0
+			;;
+	esac
+	salary=$(($EMPLOYEE_HRS*$EMPLOYEE_RATE_PER_HR))
+	totalSalary=$(($totalSalary + $salary))
+done
