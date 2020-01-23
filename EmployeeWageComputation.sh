cbf3c6fdeee2cd6627ed162385dc415b53f9a5cd
gajanan15 <gajanandesai97@gmail.com>
2020-01-23 13:27:11 +0530

    REFACTOR:Proper Indentations

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -7,14 +7,14 @@ EMPLOYEE_RATE_PER_HR=20
 employeeCheck=$((RANDOM%3))
 
 case $employeeCheck in
-			$WORKING_FULLTIME)
-						EMPLOYEE_HRS=8
-						;;
-			$WORKING_PARTTIME)
-						EMPLOYEE_HRS=4
-						;;
-			*)
-			EMPLOYEE_HRS=0
-						;;
+	$WORKING_FULLTIME)
+		EMPLOYEE_HRS=8
+		;;
+	$WORKING_PARTTIME)
+		EMPLOYEE_HRS=4
+		;;
+	*)
+		EMPLOYEE_HRS=0
+		;;
 esac
 salary=$(($EMPLOYEE_HRS*$EMPLOYEE_RATE_PER_HR))
