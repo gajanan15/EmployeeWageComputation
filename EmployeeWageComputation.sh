e510c47b098ce5742ce6fb5d5052589b335ac853
gajanan15 <gajanandesai97@gmail.com>
2020-01-23 13:13:48 +0530

    MODIFIED: rewriting code in case statement

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -5,13 +5,16 @@ WORKING_PARTTIME=1
 WORKING_FULLTIME=2
 EMPLOYEE_RATE_PER_HR=20
 employeeCheck=$((RANDOM%3))
-if [ $WORKING_FULLTIME -eq $employeeCheck ]
-then
-	EMPLOYEE_HRS=8
-elif [ $WORKING_PARTTIME -eq $employeeCheck ]
-then
-	EMPLOYEE_HRS=4
-else
-	EMPLOYEE_HRS=0
-fi
+
+case $employeeCheck in
+			$WORKING_FULLTIME)
+						EMPLOYEE_HRS=8
+						;;
+			$WORKING_PARTTIME)
+						EMPLOYEE_HRS=4
+						;;
+			*)
+			EMPLOYEE_HRS=0
+						;;
+esac
 salary=$(($EMPLOYEE_HRS*$EMPLOYEE_RATE_PER_HR))
