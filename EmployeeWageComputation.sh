d1ff9da9993d0178251d7c69bc375230224adbff
gajanan15 <gajanandesai97@gmail.com>
2020-01-23 12:59:38 +0530

    ADDED:Wage calculation of part time employee,REFACTOR:change variables

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation"
-#Constant
-EMPLOYEE_IS_PRESENT=1
-employeeCheck=$((RANDOM%2))
-if [ $EMPLOYEE_IS_PRESENT -eq $employeeCheck ]
+#constant
+WORKING_PARTTIME=1
+WORKING_FULLTIME=2
+EMPLOYEE_RATE_PER_HR=20
+employeeCheck=$((RANDOM%3))
+if [ $WORKING_FULLTIME -eq $employeeCheck ]
 then
-	EMPLOYEE_RATE_PER_HR=20
 	EMPLOYEE_HRS=8
-	salary=$(($EMPLOYEE_HRS*$EMPLOYEE_RATE_PER_HR))
+elif [ $WORKING_PARTTIME -eq $employeeCheck ]
+then
+	EMPLOYEE_HRS=4
 else
-	salary=0
+	EMPLOYEE_HRS=0
 fi
+salary=$(($EMPLOYEE_HRS*$EMPLOYEE_RATE_PER_HR))
